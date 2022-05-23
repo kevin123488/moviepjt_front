af058e78644fba8b1165751bb1dc5a84abe74937
qownstlr0394 <qownstlr0394@naver.com>
2022-05-23 22:10:55 +0900

    selectmovie

--- src/views/RecommendMovieView.vue
+++ src/views/RecommendMovieView.vue
@@ -1,27 +1,44 @@
 <template>
   <div>
     <h1>추천 영화</h1>
-    <!-- <div class="genre-select" v-if="!genre_selected">
-      <h2>장르를 선택해주세요</h2>
-      <div>첫번째 영화 칸</div>
-      <div>두번째 영화 칸</div>
-      <div>세번째 영화 칸</div>
-      <div>네번째 영화 칸</div>
-    </div> -->
-    <!-- <button @click="randomMovie"> A조</button> -->
-    <!-- <img :src="imageSrc" class="card-img-top" alt="..."> -->
     <h2>A조</h2>
-    <div class="d-inline-flex h-50 p-2 justify-content-center" v-for="movie in selectmoviesA" :key="movie.pk">
-      <div class="card px-0" style="width: 18rem;">
-        <img :src="'https://image.tmdb.org/t/p/original' + `${movie.poster_path}`" class="card-img-top" alt="...">
-        <div class="card-body">
-          {{movie.title}}
+      <!-- <div class="d-inline-flex h-50 p-2 justify-content-center" v-for="movie in selectmoviesA" :key="movie.pk">
+        <div class=" btn card px-0" style="width: 18rem;" @click="aClick">
+          <img :src="'https://image.tmdb.org/t/p/original' + `${movie.poster_path}`" class="card-img-top" alt="...">
+          <div class="card-body">
+            {{movie.title}}
+          </div>
+        </div>
+      </div> -->
+      <div class="d-inline-flex h-50 p-2 justify-content-center">
+        <div class="btn card px-0" style="width: 18rem;" @click="aClick">
+          <img :src="'https://image.tmdb.org/t/p/original' + `${selectmoviesA1.poster_path}`" class="card-img-top" alt="...">
+            <div class="card-body">
+              {{selectmoviesA1.title}}
+            </div>
+        </div>
+        <div class="btn card px-0" style="width: 18rem;" @click="aClick">
+          <img :src="'https://image.tmdb.org/t/p/original' + `${selectmoviesA2.poster_path}`" class="card-img-top" alt="...">
+            <div class="card-body">
+              {{selectmoviesA2.title}}
+            </div>
+        </div>
+        <div class="btn card px-0" style="width: 18rem;" @click="aClick">
+          <img :src="'https://image.tmdb.org/t/p/original' + `${selectmoviesA3.poster_path}`" class="card-img-top" alt="...">
+            <div class="card-body">
+              {{selectmoviesA3.title}}
+            </div>
+        </div>
+        <div class="btn card px-0" style="width: 18rem;" @click="aClick">
+          <img :src="'https://image.tmdb.org/t/p/original' + `${selectmoviesA4.poster_path}`" class="card-img-top" alt="...">
+            <div class="card-body">
+              {{selectmoviesA4.title}}
+            </div>
         </div>
       </div>
-    </div>
     <h2>B조</h2>
     <div class="d-inline-flex h-50 p-2 justify-content-center" v-for="movie in selectmoviesB" :key="movie.pk">
-      <div class="card px-0" style="width: 18rem;">
+      <div class="btn card px-0" style="width: 18rem;" @click="bClick">
         <img :src="'https://image.tmdb.org/t/p/original' + `${movie.poster_path}`" class="card-img-top" alt="...">
         <div class="card-body">
           {{movie.title}}
@@ -30,7 +47,7 @@
     </div>
     <h2>C조</h2>
     <div class="d-inline-flex h-50 p-2 justify-content-center" v-for="movie in selectmoviesC" :key="movie.pk">
-      <div class="card px-0" style="width: 18rem;">
+      <div class="btn card px-0" style="width: 18rem;" @click="cClick">
         <img :src="'https://image.tmdb.org/t/p/original' + `${movie.poster_path}`" class="card-img-top" alt="...">
         <div class="card-body">
           {{movie.title}}
@@ -39,7 +56,7 @@
     </div>
     <h2>D조</h2>
     <div class="d-inline-flex h-50 p-2 justify-content-center" v-for="movie in selectmoviesD" :key="movie.pk">
-      <div class="card px-0" style="width: 18rem;">
+      <div class="btn card px-0" style="width: 18rem;" @click="dClick">
         <img :src="'https://image.tmdb.org/t/p/original' + `${movie.poster_path}`" class="card-img-top" alt="...">
         <div class="card-body">
           {{movie.title}}
@@ -60,27 +77,97 @@ export default {
   data() {
     return {
       movies: this.$store.state.movies_db,
-      selectmoviesA: [],
+      selected : '',
+      selectmoviesA:[],
+      selectmoviesA1: {
+        title:'',
+        poster_path:'',
+        isClicked:false
+      },
+      selectmoviesA2: {
+        title:'',
+        poster_path:'',
+        isClicked:false
+      },
+      selectmoviesA3: {
+        title:'',
+        poster_path:'',
+        isClicked:false
+      },
+      selectmoviesA4: {
+        title:'',
+        poster_path:'',
+        isClicked:false
+      },
       selectmoviesB: [],
       selectmoviesC: [],
       selectmoviesD: [],
+      isClicked: false,
     }
   },
+  methods: {
+    aClick(a) {
+      this.selectmoviesA.selected = a.type
+        this.selectmoviesA.forEach(a => {
+          if (a === a) {
+            a.isClicked = !a.isClicked
+            if (!a.isClicked) {
+              this.selectmoviesA.selected = ''
+            }
+          } else {
+            a.isClicked = false
+          }
+        })
+      console.log(a.isClicked)
+    },
+    bClick() {
+      if (this.selectmoviesB.isClicked == true) {
+        this.selectmoviesB.isClicked = false
+      } else {
+        this.selectmoviesB.isClicked = true
+      }
+      console.log(this.selectmoviesB.isClicked)
+    },
+    cClick() {
+      if (this.selectmoviesC.isClicked == true) {
+        this.selectmoviesC.isClicked = false
+      } else {
+        this.selectmoviesC.isClicked = true
+      }
+      console.log(this.selectmoviesC.isClicked)
+    },
+    dClick() {
+      if (this.selectmoviesD.isClicked == true) {
+        this.selectmoviesD.isClicked = false
+      } else {
+        this.selectmoviesD.isClicked = true
+      }
+      console.log(this.selectmoviesD.isClicked)
+    },
+  },
   created() {
-    this.selectmoviesA = _.sampleSize(this.movies,4)
+    this.selectmoviesA1 = _.sample(this.movies)
+    this.selectmoviesA2 = _.sample(this.movies)
+    this.selectmoviesA3 = _.sample(this.movies)
+    this.selectmoviesA4 = _.sample(this.movies)
+    this.selectmoviesA = [this.selectmoviesA1, this.selectmoviesA2, this.selectmoviesA3, this.selectmoviesA4]
+    // this.selectmoviesA += this.selectmoviesA2
+    // this.selectmoviesA += this.selectmoviesA3
+    // this.selectmoviesA += this.selectmoviesA4
+    // this.selectmoviesA = _.sampleSize(this.movies,4)
     this.selectmoviesB = _.sampleSize(this.movies,4)
     this.selectmoviesC = _.sampleSize(this.movies,4)
     this.selectmoviesD = _.sampleSize(this.movies,4)
+    // console.log(this.selectmoviesA1)
+    // console.log(this.selectmoviesA2)
+    // console.log(this.selectmoviesA3)
+    // console.log(this.selectmoviesA4)
+    console.log(1)
     console.log(this.selectmoviesA)
-    console.log(this.selectmoviesB)
-    console.log(this.selectmoviesC)
-    console.log(this.selectmoviesD)
+    // console.log(this.selectmoviesB)
+    // console.log(this.selectmoviesC)
+    // console.log(this.selectmoviesD)
   },
-  // methods: {
-  //   randomMovie() {
-  //     this.selectmovies = _.sampleSize(this.movies,4)
-  //   }
-  // },
 }
 </script>
 
