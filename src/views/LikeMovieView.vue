b19c4211dc58e847d4027933956c2ea053313993
kevin123488 <kevin123488@naver.com>
2022-05-23 11:07:00 +0900

    commitcommit

--- src/views/LikeMovieView.vue
+++ src/views/LikeMovieView.vue
@@ -3,12 +3,12 @@
     <h1>내가 찜한 영화</h1>
     <!-- <div v-if="profile.like_movies"> -->
       <ul>
-        <li v-for="movie in profile.like_movies" :key="movie.pk">
-          <!-- <router-link :to="{ name: 'moviedetail', params: { moviePk: movie.pk } }"> -->
-            {{ movie.title }}
-          <!-- </router-link> -->
-        </li>
-      </ul>
+      <li v-for="movie in profile.like_movies" :key="movie.pk">
+        <!-- <router-link :to="{ name: 'moviedetail', params: { moviePk: movie.pk } }"> -->
+          {{ movie.title }}
+        <!-- </router-link> -->
+      </li>
+    </ul>
     <!-- </div> -->
   </div>
 </template>
@@ -24,15 +24,9 @@ export default {
   methods: {
     ...mapActions(['fetchProfile'])
   },
-  // update() {
-  //   const payload = { username: this.$route.params.username }
-  //   this.fetchProfile(payload)
-  // },
   created() {
     const payload = { username: this.$route.params.username }
     this.fetchProfile(payload)
-    // 현재 접속중인 user의 정보를 항상 admin으로 받아오는 것 같음
-    // this.$route.params.username이 어떤 로직인지 알아봐야겠음
   },
 }
 </script>
