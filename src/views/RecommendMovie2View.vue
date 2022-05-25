daf78bbc8bc7c2ff7b62b8c6af51e04d0846c1ce
kevin123488 <kevin123488@naver.com>
2022-05-25 14:39:19 +0900

    css_rec_fin

--- src/views/RecommendMovie2View.vue
+++ src/views/RecommendMovie2View.vue
@@ -1,15 +1,35 @@
 <template>
     <div>
-      <h1>결승전</h1>
-      <div v-if="!start" @click="takeMovie"><button type="button">시작하기</button></div>
+      <div class="d-flex">
+          <button v-if="!start" class="button-next" type="button" @click="takeMovie">시작하기</button>
+      </div>
       <div v-if="!!start">
-          <h1>Match Final</h1>
-          <div @click="clickFinal(movies[0])">{{ movies[0] }}</div>
-          <div @click="clickFinal(movies[1])">{{ movies[1] }}</div>
-          <div><h1>최종선택: {{ selected[selected.length-1] }}</h1></div>
-          <div v-if="isSelected" @click="finished"><button type="button">제출</button></div>
+          <div class="group"><div class="group-align"><h1>Match final</h1></div></div>
+          <div class="select"><h1>최종선택: {{ selected[selected.length-1] }}</h1></div>
+          <div class="d-flex justify-content-between matchup">
+            <div></div>
+            <div @click="clickFinal(movies[0])"><img class="img" :src="'https://image.tmdb.org/t/p/original' + poster_1" alt=""></div>
+            <div @click="clickFinal(movies[1])"><img class="img" :src="'https://image.tmdb.org/t/p/original' + poster_2" alt=""></div>
+            <div></div>
+          </div>
+
+          <div class="d-flex">
+              <button type="button" class="button-next" v-if="isSelected" @click="finished">저장</button>
+          </div>
+          <div class="d-flex">
+              <button type="button" class="button-next" data-bs-toggle="modal" data-bs-target="#exampleModal" v-if="final_submit">제출</button>
+          </div>
           <div v-if="!!poster">
-              <img :src="'https://image.tmdb.org/t/p/original' + poster" alt="">
+              <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content modal-sizeup">
+                        <div class="modal-body">
+                            <h1>champion!!</h1>
+                            <div><img class="img-final" :src="'https://image.tmdb.org/t/p/original' + poster" alt=""></div>
+                        </div>
+                    </div>
+                </div>
+              </div>
           </div>
       </div>
     </div>
@@ -25,16 +45,29 @@ export default {
             isSelected: false,
             selected: [],
             poster: '',
+            poster_1: '',
+            poster_2: '',
+            final_submit: false,
         }
     },
     methods: {
         takeMovie: function() {
             this.movies = this.$store.state.movie2Top
             this.start = true
+            const movie1 = this.movies[0]
+            const movie2 = this.movies[1]
+            this.$store.state.movies_db.forEach(res => {
+                if (res.title === movie1) {
+                    this.poster_1 = res.poster_path
+                } else if (res.title === movie2) {
+                    this.poster_2 = res.poster_path
+                }
+            })
         },
         clickFinal: function(movie) {
             this.selected.push(movie)
             this.isSelected = true
+            this.final_submit = false
         },
         finished: function() {
             const movieSelected = this.selected[this.selected.length-1]
@@ -44,11 +77,27 @@ export default {
                 }
             })
             console.log(this.poster)
+            this.isSelected = false
+            this.final_submit = true
+            this.selected = []
         }
     },
 }
 </script>
 
 <style>
+
+.modal-sizeup {
+    height: 700px;
+    width: 600;
+    border-radius: 50px;
+    box-shadow: 5px 5px 5px 5px;
+}
+
+.img-final {
+    height: 600px;
+    width: 460px;
+    border-radius: 50px;
+}
 
 </style>
--- src/views/RecommendMovie4View.vue
+++ src/views/RecommendMovie4View.vue
@@ -1,26 +1,35 @@
 <template>
   <div>
-    <h1>4강 입니다</h1>
-    <button v-if="!start" type="button" @click="takeMovie">4강 시작하기</button>
+      <div class="d-flex">
+        <button v-if="!start" class="button-next" type="button" @click="takeMovie">4강 시작하기</button>
+      </div>
     <div v-if="!!start">
         <div>
-            <h1>match 1</h1>
-            <div class="d-flex"><h1>선택중</h1>{{ clickA.movie[0] }}</div>
-            <div @click="click1(movies[0])">{{ movies[0] }}</div>
-            <div @click="click1(movies[2])">{{ movies[2] }}</div>
+            <div class="group"><div class="group-align"><h1>Match 1</h1></div></div>
+            <div class="select">{{ clickA.movie[0] }}</div>
+            <div class="d-flex justify-content-between matchup">
+                <div></div>
+                <div @click="click1(movies[0])"><img class="img" :src="'https://image.tmdb.org/t/p/original' + poster_1" alt=""></div>
+                <div @click="click1(movies[2])"><img class="img" :src="'https://image.tmdb.org/t/p/original' + poster_3" alt=""></div>
+                <div></div>
+            </div>
         </div>
         <div>
-            <h1>match 2</h1>
-            <div class="d-flex"><h1>선택중</h1>{{ clickB.movie[0] }}</div>
-            <div @click="click2(movies[1])">{{ movies[1] }}</div>
-            <div @click="click2(movies[3])">{{ movies[3] }}</div>
+            <div class="group"><div class="group-align"><h1>Match 2</h1></div></div>
+            <div class="select">{{ clickB.movie[0] }}</div>
+            <div class="d-flex justify-content-between matchup">
+                <div></div>
+                <div @click="click2(movies[1])"><img class="img" :src="'https://image.tmdb.org/t/p/original' + poster_2" alt=""></div>
+                <div @click="click2(movies[3])"><img class="img" :src="'https://image.tmdb.org/t/p/original' + poster_4" alt=""></div>
+                <div></div>
+            </div>
         </div>
-        <div>
-            <button type="button" @click="gogogo">저장하기(낙장불입)</button>
+        <div class="d-flex">
+            <button type="button" class="button-next" v-if="!goNext" @click="gogogo">저장하기</button>
         </div>
-        <div>
-            <button type="button" v-if="goNext" @click="movieSave">
-                <router-link to="/recommendmovie/2top">
+        <div class="d-flex">
+            <button type="button" class="button-next" v-if="goNext" @click="movieSave">
+                <router-link style="color: white;" to="/recommendmovie/2top">
                     다음으로
                 </router-link>
             </button>
@@ -46,12 +55,31 @@ export default {
             },
             selected: [],
             goNext: false,
+            poster_1: '',
+            poster_2: '',
+            poster_3: '',
+            poster_4: '',
         }
     },
     methods: {
         takeMovie: function() {
             this.movies = this.$store.state.movie4Top
             this.start = true
+            const movie1 = this.movies[0]
+            const movie2 = this.movies[1]
+            const movie3 = this.movies[2]
+            const movie4 = this.movies[3]
+            this.$store.state.movies_db.forEach(res => {
+                if (res.title === movie1) {
+                    this.poster_1 = res.poster_path
+                } else if (res.title === movie2) {
+                    this.poster_2 = res.poster_path
+                } else if (res.title === movie3) {
+                    this.poster_3 = res.poster_path
+                } else if (res.title === movie4) {
+                    this.poster_4 = res.poster_path
+                }
+            })
         },
         click1: function(movie) {
             if (this.clickA.isClicked === 0) {
